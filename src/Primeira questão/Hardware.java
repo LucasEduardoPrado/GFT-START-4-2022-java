c50c40d70714f16d8fa1fe5c3d0858670f433c17
LucasEduardoPrado <lukasedu635@gmail.com>
2022-04-01 17:59:15 -0300

    avaliação

---
+++ src/Primeira questão/Hardware.java
@@ -0,0 +1,43 @@
+public class Hardware {
+    public id (String id){
+        this.setid (id );
+    }
+    public String getid () {
+        return id ;
+    }
+    
+    public void setdescrição (String id ) {
+        this.id  = id ;
+    }
+    
+    public descrição(String descrição){
+        this.setdescrição (descrição );
+    }
+    public String getidescrição () {
+        return descrição;
+    }
+    
+    public void setvalor (String valor ) {
+        this.valor  = valor ;
+    }
+    public valor(String valor){
+        this.setvalor (valor );
+    }
+    public String getvalor() {
+        return valor ;
+    }
+    
+    public void setfabricante  (String fabricante ) {
+        this.fabricante   = fabricante  ;
+    }
+    public fabricante  (String fabricante ){
+        this.setfabricante  (fabricante  );
+    }
+    public String getid () {
+        return fabricante  ;
+    }
+    
+    public void setfabricante (String fabricante ) {
+        this.fabricante   = fabricante ;
+    }
+}
--- src/Terceira questão/Pessoa.java
+++ src/Terceira questão/Pessoa.java
@@ -2,10 +2,41 @@ public class Pessoa {
   public static void main(String[] args) {
     Scanner leitor = new Scanner(System.in);
     String nome =  leitor.nextDouble();
-    char sexo = leitor.nextDouble();
+    String sexo = leitor.nextDouble();
     Date dataNascimento = leitor.nextDouble();
 
-    
+    System.out.printf("Informe o seu ((F)Feminino ou (M)Masculino): ");
+      sexo = in.next();
+
+      if(sexo == "f" || sexo == "F"){
+        return sexo;
+      }
+      if(sexo == "m" || sexo == "M"){
+        return sexo;
+      }
+    }
+
+    public static int dataNascimento(Date data) {
+      Calendar cData = Calendar.getInstance();
+      Calendar cHoje= Calendar.getInstance();
+      cData.setTime(data);
+      cData.set(Calendar.YEAR, cHoje.get(Calendar.YEAR));
+      int idade = cData.after(cHoje) ? -1 : 0;
+      cData.setTime(data);
+      idade += cHoje.get(Calendar.YEAR) - cData.get(Calendar.YEAR);
+      return idade;
+    }
+    public void imprimeTempoVidaRestante() {
+      if (sexo == "m" || sexo == "M"){
+        resultado = idade - 73;
+      }
+      else if (sexo == "f" || sexo == "F"){
+        resultado = idade - 80;
+      }
+      
+    }
+
+    System.out.println(resultado);
 
     
 
