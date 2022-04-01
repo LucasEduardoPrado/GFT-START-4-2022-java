7a4b25a6e6ebefe1f5aedf563970a6bdf4c8bad0
LucasEduardoPrado <lukasedu635@gmail.com>
2022-04-01 17:25:46 -0300

    vetor

---
+++ src/Segunda questão/Vetor.java
@@ -0,0 +1,32 @@
+public class Vetor {
+    public static void main(String[] args){    int vetor[];
+        Scanner leitor = new Scanner(System.in);
+    
+       
+        for (i = 0; i < 10; i++) {
+            System.out.System.out.println(); ("Digite um numero: ");
+            leitor ("%d", &x[i]);
+        }
+       
+        for (i = 0; i < 10; i++){
+            for(j = i+1; j < 10; j++) {
+                if (x[i] > x[j]) {
+                    temp = x[i];
+                    x[i] = x[j];
+                    x[j] = temp;
+                }
+            }
+      
+        }
+
+        for (i=0 ; i < 10; i++) {
+            linha = br.readLine();
+            n = Double.parseDouble(linha);
+            if(n > 0.0)Pos++;
+        }
+
+        System.out.println("" + Pos + " valores positivos");
+    }
+
+
+}
---
+++ src/Terceira questão/Pessoa.java
@@ -0,0 +1,15 @@
+public class Pessoa {
+  public static void main(String[] args) {
+    Scanner leitor = new Scanner(System.in);
+    String nome =  leitor.nextDouble();
+    char sexo = leitor.nextDouble();
+    Date dataNascimento = leitor.nextDouble();
+
+    
+
+    
+
+    
+  }
+    
+}
